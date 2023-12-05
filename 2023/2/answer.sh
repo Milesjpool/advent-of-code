44e2028c16cad0e7635d6cc0e5fdebe1baae9602
Miles Pool <miles@landinsight.io>
2023-12-05 22:51:46 +0000

    Update answer.sh

--- 2023/2/answer.sh
+++ 2023/2/answer.sh
@@ -1,2 +1,2 @@
-cat temp.txt | grep -v -e "\(1[3-9]\|[2-9][0-9]\) red" | grep -v -e "\(1[4-9]\|[2-9][0-9]\) green" | grep -v -e "\(1[5-9]\|[2-9][0-9]\) blue" | sed -re 's/Game ([0-9]+): .*/\1/' | paste -sd+ - | bc                       
+cat input.txt | grep -v -e "\(1[3-9]\|[2-9][0-9]\) red" | grep -v -e "\(1[4-9]\|[2-9][0-9]\) green" | grep -v -e "\(1[5-9]\|[2-9][0-9]\) blue" | sed -re 's/Game ([0-9]+): .*/\1/' | paste -sd+ - | bc                       
 2617
